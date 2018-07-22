0d0a04e12669121db20169f632136b49090b9513
annafagocistka <anafagocistka@gmail.com>
2018-07-22 02:55:51 +0200

    wynik

--- main.js
+++ main.js
@@ -1,8 +1,13 @@
-var num1 = 2;
-var num2 = 5;
+let num1 = 2;
+let num2 = 5;
+function porownanie(num1,num2){
+    
  if (2 > 5) {
      console.log("liczba 2 jest wieksza od 5");
 }
 else {
     console.log("liczba 5 jest wieksza od 2");
 }
+    }
+
+console.log(porownanie(num1,num2));
